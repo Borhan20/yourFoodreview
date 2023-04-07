4a1ce1ba321b3fa1da19fca749a62244511e216f
Borhan20 <borhanuddin.24csedu.020@gmail.com>
2023-04-07 17:14:17 +0600

    rendering

--- viaFoodReview/viaFoodReview/urls.py
+++ viaFoodReview/viaFoodReview/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,6 +39,8 @@ urlpatterns = [
           name='password_reset_complete'),
     path('', include('blog.urls')),
 ]
+
+urlpatterns+=staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
