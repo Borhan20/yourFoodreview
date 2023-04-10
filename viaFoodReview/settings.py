02301ca7152cd639026b3c3e70c2333c20b41b80
Borhan20 <borhanuddin.24csedu.020@gmail.com>
2023-04-11 05:06:18 +0600

    correction debug

--- viaFoodReview/settings.py
+++ viaFoodReview/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pul8)%r2b8qi_(n=f0)i(*g=hff^e095_jas(nm*uaur_yue%c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ['*']
 
