b3d3aa445f7309204f7f4c6b62e4d8946d870468
Borhan20 <borhanuddin.24csedu.020@gmail.com>
2023-04-11 13:49:04 +0600

    test

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
@@ -24,7 +25,9 @@ urlpatterns = [
     
 ]
 
+urlpatterns+=staticfiles_urlpatterns()
 
-urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #<app>/<model>_<viewtype>.html
