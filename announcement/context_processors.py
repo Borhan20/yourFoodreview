11499ad24d91af416cbe34844ac7155af85a4799
Borhan20 <borhanuddin.24csedu.020@gmail.com>
2023-05-14 17:13:41 +0600

    comment issue fixed

--- announcement/context_processors.py
+++ announcement/context_processors.py
@@ -8,7 +8,7 @@ def message_processor(request):
 
     
     announcement = Announcement.objects.all()
-    paginator = Paginator(announcement, 5) 
+    paginator = Paginator(announcement, 10) 
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
