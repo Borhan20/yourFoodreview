7c3e8893f88016a804ab21ff8b807241da188fca
Borhan20 <borhanuddin.24csedu.020@gmail.com>
2024-02-03 16:57:21 +0600

    change database to sqlite3

--- blog/urls.py
+++ blog/urls.py
@@ -3,17 +3,14 @@ from django.contrib import admin
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
                     PostDeleteView,
-                    UserPostListView,
-                    ReplyCommentView,
-                
+                    UserPostListView
                     )
-from .views import like_post
+
 
 urlpatterns = [
     path('', PostListView.as_view(),name='blog-home'),
@@ -23,12 +20,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(),name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(),name='post-delete'),
     path('about/',views.about, name='blog-about'),
-    path('like/<int:pk>/', views.like_post, name='like_post'),
-    path('comment/<int:pk>/', ReplyCommentView.as_view(), name='reply_comment'),
-    
 ]
-
-urlpatterns+=staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- usermessage/models.py
+++ usermessage/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django import forms
 
-class Message(models.Model):
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+# Create your models here.
+
+class UserMessage(models.Model):
+    message = models.CharField(max_length=500)
+    user= models.ForeignKey(User,on_delete=models.CASCADE)
+    date_messaged = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-date_messaged']
+
+
+    def __str__(self):
+        return self.message
