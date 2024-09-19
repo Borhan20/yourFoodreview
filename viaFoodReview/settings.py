fcaed98eb8690f4b0d4c2e2f9fc03b55289311f0
Borhan20 <borhanuddin.24csedu.020@gmail.com>
2024-09-19 12:11:54 +0600

    fixed issue

--- viaFoodReview/settings.py
+++ viaFoodReview/settings.py
@@ -92,15 +92,15 @@ WSGI_APPLICATION = 'viaFoodReview.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#     'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_URL')
-EMAIL_HOST_PASSWORD = os.environ.get('PASS_URL')
-# EMAIL_HOST_USER = 'viafoodreview@gmail.com'
-# EMAIL_HOST_PASSWORD = 'lhgvpkhitbjjcool'
+# EMAIL_HOST_USER = os.environ.get('EMAIL_URL')
+# EMAIL_HOST_PASSWORD = os.environ.get('PASS_URL')
+EMAIL_HOST_USER = 'viafoodreview@gmail.com'
+EMAIL_HOST_PASSWORD = 'lhgvpkhitbjjcool'
 DEFAULT_FROM_EMAIL = 'Coding with weak team <no reply@weakteam.com>'
